13d9f296d9f8e13d2213fb75a82f0a382222ea75
Ella Tanttu <ellajtanttu@gmail.com>
2022-01-07 16:06:25 -0800

    add table models

---
+++ Factory/Models/Engineer.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+namespace FactoryManager.Models
+{
+  public class Engineer
+  {
+    public Engineer()
+    {
+      this.MachineEntities = new HashSet<EngineerMachine>();
+    }
+    public int EngineerId {get; set;}
+    public string Name {get; set;}
+    public virtual ICollection<EngineerMachine> MachineEntities {get;set;}
+  }
+}
