41b0711e71e96e9b0e72c6114fb947670c67c1cc
Ella Tanttu <ellajtanttu@gmail.com>
2022-01-07 16:14:44 -0800

    add dbContext, projectManagerContext, and join model

---
+++ Factory/Models/DesignTimeDbContextFactory.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace FactoryManager.Models
+{
+  public class FactoryManagerContextFactory : IDesignTimeDbContextFactory<FactoryManagerContext>
+  {
+    FactoryManagerContext IDesignTimeDbContextFactory<FactoryManagerContext>.CreateDbContext(string[] args)
+    {
+      IConfigurationRoot configuration = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json")
+        .Build();
+
+        var builder = new DbContextOptionsBuilder<FactoryManagerContext>();
+
+        builder.UseMySql(configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(configuration["ConnectionStrings:DefaultConnection"]));
+
+        return new FactoryManagerContext(builder.Options);
+    }
+  }
+}
